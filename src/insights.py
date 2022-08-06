dc0b1c0f13df12cc0181401523448cd4084f119c
CrisSouzaMA <cris.souzamoreiraealmeida@gmail.com>
2022-08-06 18:21:33 -0300

    Feat: implementa função que irá retornar uma lista com os empregos filtrados pelo tipo de industria

--- src/insights.py
+++ src/insights.py
@@ -30,21 +30,12 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    list_industry_type = list()
+    for industrie in jobs:
+        if (industrie["industry"] == industry):
+            list_industry_type.append(industrie)
+    
+    return list_industry_type
 
 
 def get_max_salary(path):
