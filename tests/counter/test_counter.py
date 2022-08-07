1204a984f0c89783c1fcb1fc09930b7d5345e582
CrisSouzaMA <cris.souzamoreiraealmeida@gmail.com>
2022-08-07 14:47:25 -0300

    Feat: implementa teste para validar função count_ocurrences do arquivo counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    file = 'src/jobs.csv'
+    WORDS = ["Javascript", "Python"]
+    JS_COUNT = 122
+    PYTHON_COUNT = 1639
+    assert count_ocurrences(file, WORDS[0]) == JS_COUNT
+    assert count_ocurrences(file, WORDS[1]) == PYTHON_COUNT
