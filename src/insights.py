3cf7f5e7683de44bd93db80f7d67bebfb461f658
CrisSouzaMA <cris.souzamoreiraealmeida@gmail.com>
2022-08-07 12:25:05 -0300

    Feat: implementa função que vai fazer o filtro de empregos por salario

--- src/insights.py
+++ src/insights.py
@@ -79,18 +79,12 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
+    list_jobs = list()
+    for job in jobs:
+        try:
+            if (matches_salary_range(job, salary)):
+                list_jobs.append(job)
+        except ValueError:
+            continue
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    return list_jobs
