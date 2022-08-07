d4362dab4d0272db6fa1df875dbee54245f96150
CrisSouzaMA <cris.souzamoreiraealmeida@gmail.com>
2022-08-07 14:54:04 -0300

    Feat: implementa teste para checar se a função read_brazilian_file no arquivo brazilian_jobs está funcionando corretamente

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    PATH = 'tests/mocks/brazilians_jobs.csv'
+    report = read_brazilian_file(PATH)
+    for key in report:
+        assert "title" in key
+        assert "salary" in key
+        assert "type" in key
